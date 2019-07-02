2aa16f5a7387f444f3be2695be38819b3e06b51f
Brently Bazor <brently.bazor@gmail.com>
2019-07-02 00:48:14 -0500

    updated about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,7 +4,12 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return(
-      <div><h1>About Page</h1></div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+        <div style={{ }}>
+          <h1>About Page</h1>
+          <p style={{ textAlign: 'center' }}>Coming Soon...</p>
+        </div>
+      </div>
     )
   }
 }
