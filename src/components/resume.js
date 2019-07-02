d8a8662a1c54289650156e90379dfef353021454
Brently Bazor <brently.bazor@gmail.com>
2019-07-02 00:36:06 -0500

    added unordered interest list

--- src/components/resume.js
+++ src/components/resume.js
@@ -22,7 +22,13 @@ class Resume extends Component {
             <h2 style={{paddingTop: '2em'}}>Brently Bazor</h2>
             <h4 style={{color: 'grey'}}>Programmer</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <ul>
+              <li>Father</li>
+              <li>Gamer</li>
+              <li>Programmer</li>
+              <li>Ultimate Frisbee</li>
+              <li>Martial Artist</li>
+            </ul>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <h5>Address</h5>
             <p>2001 Carterville Road</p>
