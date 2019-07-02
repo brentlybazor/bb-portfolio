cba0fe21b865c1fcf9d223ebbeeecb60d4db599d
Brently Bazor <brently.bazor@gmail.com>
2019-07-01 20:27:57 -0500

    Image update

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -9,7 +9,7 @@ class Landing extends Component {
         <Grid className="landing-grid">
           <Cell col={12}>
             <img
-              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+              src="images/bitmoji-forward.png"
               alt="avatar"
               className="avatar-img"
               />
