e8c935e7517102caff32d9e3f56128cca293aa6f
Brently Bazor <brently.bazor@gmail.com>
2019-07-12 22:16:26 +0000

    update

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,7 +5,7 @@ import { Grid, Cell } from 'react-mdl';
 class Landing extends Component {
   render() {
     return(
-      <div style={{width: '100%', margin: 'auto'}}>
+      <div style={{width: '100%'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
             <img
