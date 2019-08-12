144a7f7a1728955a2516315e3045d393fc183f1b
Brently Bazor <brently.bazor@gmail.com>
2019-08-11 23:54:42 -0700

    updated projects
    added a react project to there page

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Brently Bazor</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Brently Bazor</Link>}>
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
+        <Layout>
+            <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Brently Bazor</Link>} scroll>
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Header>
+            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Brently Bazor</Link>}>
+                <Navigation>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/aboutme">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+                <Main/>
+            </Content>
+        </Layout>
+      </div>
 
     );
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -12,26 +12,24 @@ class Projects extends Component {
 
     if(this.state.activeTab === 0){
       return(
-        // <div className="projects-grid">
         <Grid>
-          <h3 style={{ justifyContent: 'center'}}>Projects page is coming soon...</h3>
-          <br/>
+          {/* <h3 style={{ justifyContent: 'center'}}>Projects page is coming soon...</h3>
+          <br/> */}
 
           {/* Project 1 */}
-          {/* <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(images/recipe-image.jpg) center / cover'}} >React Project #1</CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              Simple React app using the Edemam API.
             </CardText>
             <CardActions border style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
-              <Button colored href="http://github.com" target="_blank">GitHub</Button>
-              <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
+              <Button colored href="https://github.com/brentlybazor/recipe-app/" target="_blank">GitHub</Button>
+              <Button colored href="https://bb-recipe-api-app.netlify.com/" target="_blank">Live Demo</Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
             </CardMenu>
-          </Card> */}
+          </Card>
 
           {/* Project 2 */}
           {/* <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
@@ -49,23 +47,38 @@ class Projects extends Component {
             </CardMenu>
           </Card> */}
         </Grid>
-        // </div>
 
 
       )
-    } else if(this.state.activeTab === 1) {
+    } else {
+    // else if(this.state.activeTab === 1) {
       return (
-        <div><h1>This is Angular</h1></div>
-      )
-    } else if(this.state.activeTab === 2) {
-      return (
-        <div><h1>This is VueJS</h1></div>
-      )
-    } else if(this.state.activeTab === 3) {
-      return (
-        <div><h1>This is MongoDB</h1></div>
+
+        <Grid className="contact-grid">
+          <Cell col={12}>
+            <img
+              src="images/bitmoji-oops.jpg"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+               <h2>More Projects Coming Soon...</h2>
+
+             {/* <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p> */}
+
+          </Cell>
+        </Grid>
+
       )
     }
+    // else if(this.state.activeTab === 2) {
+    //   return (
+    //     <div><h1>This is VueJS</h1></div>
+    //   )
+    // } else if(this.state.activeTab === 3) {
+    //   return (
+    //     <div><h1>This is MongoDB</h1></div>
+    //   )
+    // }
 
   }
 
@@ -75,7 +88,9 @@ class Projects extends Component {
     return(
       <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          {/* <Tab>React</Tab> */}
+          <Tab>React</Tab>
+          <Tab>React Native</Tab>
+          <Tab>Python</Tab>
         </Tabs>
 
 
