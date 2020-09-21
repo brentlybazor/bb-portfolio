e265611f62a5072a8b596940e04c7e232dd53490
Brently Bazor <brently.bazor@gmail.com>
2020-09-21 13:20:56 -0400

    add show buttons to person info

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import './contact.css'
 
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showNumber: false,
+      showEmail: false,
+      showSkype: false
+    }
+  }
   render() {
-    return(
+    return (
       <div className="contact-body">
         <Grid className="contact-grid">
           <Cell col={6}>
@@ -12,46 +21,53 @@ class Contact extends Component {
             <img
               src="images/bitmoji-forward.png"
               alt="avatar"
-              style={{height: '250px'}}
-               />
-             {/* <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p> */}
+              style={{ height: '250px' }}
+            />
+            {/* <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p> */}
 
           </Cell>
           <Cell col={6}>
             <h2>Contact Me</h2>
-            <hr/>
+            <hr />
 
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
                     <a href={'https://brentlybazor.appointlet.com/b/brently-bazor'} style={{ color: 'black', textDecoration: 'none' }} target="_blank">
-                      <i className="fa fa-calendar" aria-hidden="true"/>
-                      <span style={{ paddingTop: 5}}>Click to Schedule Appointment</span>
+                      <i className="fa fa-calendar" aria-hidden="true" />
+                      <span style={{ paddingTop: 5 }}>Click to Schedule Appointment</span>
                     </a>
                   </ListItemContent>
                 </ListItem>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i style={{ position: 'relative', left: 0 }} className="fa fa-phone-square" aria-hidden="true"/>
-                    <span>(601) 447-0613</span>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i style={{ position: 'relative', left: 0 }} className="fa fa-phone-square" aria-hidden="true" />
+                    {!this.state.showNumber
+                      ? <button className={"showButton"} onClick={() => this.setState({ showNumber: !this.state.showNumber })}>Show Number</button>
+                      : <span>(601) 447-0613</span>}
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    <span>brently.bazor@gmail.com</span>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    {!this.state.showEmail
+                      ? <button className={"showButton"} onClick={() => this.setState({ showEmail: !this.state.showEmail })}>Show Email</button>
+                      : <span>brently.bazor@gmail.com</span>}
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-skype" aria-hidden="true"/>
-                    <span>brently.bazor</span>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    {!this.state.showSkype
+                      ? <button className={"showButton"} onClick={() => this.setState({ showSkype: !this.state.showSkype })}>Show Skype</button>
+                      : <span>brently.bazor</span>
+                    }
                   </ListItemContent>
                 </ListItem>
-              {/* <ListItem>
+                {/* <ListItem>
                 <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
                   <a href={'https://calendar.google.com/calendar/embed?src=brently.bazor@gmail.com&mode=WEEK'} style={{ color: 'black', textDecoration: 'none' }} target="_blank">
                     <i className="fa fa-calendar" aria-hidden="true"/>
@@ -59,7 +75,7 @@ class Contact extends Component {
                   </a>
                 </ListItemContent>
               </ListItem> */}
-            </List>
+              </List>
             </div>
           </Cell>
         </Grid>
