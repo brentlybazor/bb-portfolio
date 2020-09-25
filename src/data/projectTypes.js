4ffb9b16665d288624ca0107a8e24da441f4b00a
Brently Bazor <brently.bazor@gmail.com>
2020-09-25 05:30:20 -0400

    update projects

---
+++ src/data/projectTypes.js
@@ -0,0 +1,24 @@
+const projectTypes = [
+    {
+      name: 'All',
+      type: '',
+    },
+    {
+      name: 'React',
+      type: 'react',
+    },
+    {
+      name: 'React Native',
+      type: 'react-native',
+    },
+    {
+      name: 'Python',
+      type: 'python',
+    },
+    {
+        name: 'Node.js',
+        type: 'node',
+    },
+  ]
+
+  export default projectTypes;
---
+++ src/data/projects.js
@@ -0,0 +1,27 @@
+const projectsData = [
+    {
+        name:"Recipe App",
+        githubLink:"https://github.com/brentlybazor/recipe-app/",
+        liveDemoLink:"https://bb-recipe-api-app.netlify.app/",
+        image:"images/recipe-photo.jpg",
+        type:'react',
+        description:"Simple React app using the Edemam API."
+    },
+    {
+        name:"Portfolio Website",
+        liveDemoLink:"https://brentlybazor.com/",
+        image:"images/portfolio-photo.jpg",
+        type:'react',
+        description:"Person portfolio website"
+    },
+    {
+        name:"Quiz Web App",
+        githubLink:"https://github.com/brentlybazor/Trivia-Quiz-React-App",
+        liveDemoLink:"https://bb-quiz.netlify.app/",
+        image:"images/quiz-photo.jpg",
+        type:'react',
+        description:"Quiz app using API for questions."
+    },
+]
+
+export default projectsData;
