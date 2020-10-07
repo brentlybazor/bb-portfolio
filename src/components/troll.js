3bc28beae8ff0a1783caf1355656ede3e220708a
Brently Bazor <brently.bazor@gmail.com>
2020-10-07 08:45:28 -0400

    added troll page for of

---
+++ src/components/troll.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Troll() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src="images/troll-face.png"
+        alt="troll face"
+        style={{ height: "250px" }}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Troll from "./components/troll";
 // import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const renderApp = Component => {
   ReactDOM.render(
     <BrowserRouter>
-      <Component />
-    </BrowserRouter>
-    , document.getElementById('root'));
+      <Switch>
+        <Route path="/onlyfans">
+          <Troll />
+        </Route>
+        <Route path="/">
+          <Component />
+        </Route>
+      </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
   // registerServiceWorker();
 }
 
